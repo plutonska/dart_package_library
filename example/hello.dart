b9025be9ee9da0ac247093d41403aaba86790c99
Hendri <wendimivo96@gmail.com>
2023-09-18 09:08:36 +0700

    dart package library

---
+++ example/hello.dart
@@ -0,0 +1,13 @@
+import 'package:dart_package_library/hello.dart';
+
+void main(List<String> args) {
+  print(sayHello('Hendri'));
+
+  var costumer = Costumer();
+  var category = Category();
+  var product = Product();
+
+  print(costumer);
+  print(category);
+  print(product);
+}
---
+++ lib/hello.dart
@@ -0,0 +1,5 @@
+library dart_package_library;
+
+export 'package:dart_package_library/src/say_hello.dart';
+
+export 'package:dart_package_library/src/costumer.dart' show Costumer, Category, Product;
